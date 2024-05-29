62a826089da389f3ad48bcb04d0c03f9bc2c86d1
Nathan <nathan.manera@gmail.com>
2024-05-29 11:54:02 -0300

    Entrega do Desafio

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        final Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("\nNome Cliente:");
+        final String nomeCliente = scanner.nextLine();
+
+        System.out.println("\nAgência:");
+        final String agencia = scanner.nextLine();
+
+        System.out.println("\nNúmero da Conta:");
+        final int numeroConta = scanner.nextInt();
+
+        System.out.println("\nSaldo:");
+        final double saldo = scanner.nextDouble();
+
+        System.out.println("\nOlá " + nomeCliente
+                + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
+                + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+        scanner.close();
     }
 }
